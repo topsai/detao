feb22223eacbfd9ae972fbe83ff4629f51f8b857
hurte@foxmail.com <hurte@foxmail.com>
2018-02-10 00:08:50 +0800

    up

---
+++ detao/asgi.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "范斯特罗夫斯基"
+# Email: hurte@foxmail.com
+# Date: 2018/2/10
+
+import os
+from channels.asgi import get_channel_layer
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_project.settings")
+
+channel_layer = get_channel_layer()
