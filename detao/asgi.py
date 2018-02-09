13f69c1e8c1561f70e15e61e9544029c5c6409ac
hurte@foxmail.com <hurte@foxmail.com>
2018-02-10 00:10:56 +0800

    up

--- detao/asgi.py
+++ detao/asgi.py
@@ -7,6 +7,6 @@
 import os
 from channels.asgi import get_channel_layer
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "detao.settings")
 
 channel_layer = get_channel_layer()
