29c3928fd82fc1b918af6292d28b8a843768e5ec
hurte@foxmail.com <hurte@foxmail.com>
2018-02-11 10:19:20 +0800

    up

---
+++ Answer/zhengshitiku.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "范斯特罗夫斯基"
+# Email: hurte@foxmail.com
+# Date: 2018/2/11
+
+data = [
+    {
+        'round': 0,
+        'qas': [
+            {'title': '以下哪种水果是热带水果？', 'answers': ['苹果', '桃子', '香蕉'], 'correctAnswer': 2},
+            {'title': '我国共有几个直辖市？', 'answers': ['3', '4', '5'], 'correctAnswer': 1},
+            {'title': '德稻全球创新网络的英文简称是什么？', 'answers': ['GIN', 'GIG', 'CIN'], 'correctAnswer': 0},
+            {'title': '《孔乙己》的作者是哪位？', 'answers': ['鲁迅', '老舍', '郭沫若'], 'correctAnswer': 0},
+            {'title': '花甲之年指的是多少岁', 'answers': ['50岁', '60岁', '70岁'], 'correctAnswer': 1},
+            {'title': '以下哪个岛屿是泰国的？', 'answers': ['沙巴', '苏梅岛', '长滩岛'], 'correctAnswer': 1},
+            {'title': '下列哪句是李白的诗句？', 'answers': ['竹外桃花三两枝,春江水暖鸭先知', '葡萄美酒夜光杯,欲饮琵琶马上催', '孤帆远影碧空尽,唯见长江天际流'],
+             'correctAnswer': 2},
+            {'title': '湖南省因位于哪个湖以南而得名？', 'answers': ['洞庭湖', '鄱阳湖', '镜泊湖'], 'correctAnswer': 0},
+            {'title': '动漫《哆啦A梦》中，哆啦A梦的身体最初是什么颜色?', 'answers': ['蓝色', '黄色', '白色'], 'correctAnswer': 1},
+            {'title': '下列哪一部不是李安导演的作品？', 'answers': ['风月', '少年派的奇幻漂流', '断背山'], 'correctAnswer': 0},
+            {'title': '“中庸之道”是哪个流派的理论？', 'answers': ['儒家', '道家', '法家'], 'correctAnswer': 0},
+            {'title': '下列哪一项不属于声音的特性？', 'answers': ['声速', '响度', '音调'], 'correctAnswer': 0},
+        ]
+    },
+
+    {
+        'round': 1,
+        'qas': [
+            {'title': '以下哪种水果是热带水果？', 'answers': ['苹果', '桃子', '香蕉'], 'correctAnswer': 2},
+            {'title': '我国共有几个直辖市？', 'answers': ['3', '4', '5'], 'correctAnswer': 1},
+            {'title': '德稻全球创新网络的英文简称是什么？', 'answers': ['GIN', 'GIG', 'CIN'], 'correctAnswer': 0},
+            {'title': '《孔乙己》的作者是哪位？', 'answers': ['鲁迅', '老舍', '郭沫若'], 'correctAnswer': 0},
+            {'title': '花甲之年指的是多少岁', 'answers': ['50岁', '60岁', '70岁'], 'correctAnswer': 1},
+            {'title': '以下哪个岛屿是泰国的？', 'answers': ['沙巴', '苏梅岛', '长滩岛'], 'correctAnswer': 1},
+            {'title': '下列哪句是李白的诗句？', 'answers': ['竹外桃花三两枝,春江水暖鸭先知', '葡萄美酒夜光杯,欲饮琵琶马上催', '孤帆远影碧空尽,唯见长江天际流'],
+             'correctAnswer': 2},
+            {'title': '湖南省因位于哪个湖以南而得名？', 'answers': ['洞庭湖', '鄱阳湖', '镜泊湖'], 'correctAnswer': 0},
+            {'title': '动漫《哆啦A梦》中，哆啦A梦的身体最初是什么颜色?', 'answers': ['蓝色', '黄色', '白色'], 'correctAnswer': 1},
+            {'title': '下列哪一部不是李安导演的作品？', 'answers': ['风月', '少年派的奇幻漂流', '断背山'], 'correctAnswer': 0},
+            {'title': '“中庸之道”是哪个流派的理论？', 'answers': ['儒家', '道家', '法家'], 'correctAnswer': 0},
+            {'title': '下列哪一项不属于声音的特性？', 'answers': ['声速', '响度', '音调'], 'correctAnswer': 0},
+        ]
+    },
+]
